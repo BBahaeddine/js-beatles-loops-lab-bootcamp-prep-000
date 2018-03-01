bbb2853851ed5a6eecde6fafc7829b309b7b2dac
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-03-01 15:44:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ function theBeatlesPlay(musicians, instruments) {
   
   return myArray;
 }
+
+function johnLennonFacts(facts) {
+  for (let i = 0; i < facts.length; i++) {
+    facts[i] += '!!!';
+  }
+  
+  return facts;
+}
